63067b6da53a79c6e6a18bb5a76e6c3ac668ea1c
Gabriel de Sousa <gabrieldesousa@gabriels-mbp.lan>
2021-12-06 13:40:12 -0800

    created controllers for companies, positions, applicants and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :companies, only: [:new, :edit, :update, :create]
+  resources :positions
+  resources :applicants, only: [:index, :new, :create]
   devise_for :users
   root "home#index"
 end
