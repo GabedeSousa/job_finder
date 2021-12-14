c73c51cea646329e46cf789532a9448b34cb75d7
GabedeSousa <gabriel.fidelis.sousa@gmail.com>
2021-12-14 15:39:49 -0800

    views helper refactor

--- app/helpers/positions_helper.rb
+++ app/helpers/positions_helper.rb
@@ -1,2 +1,9 @@
 module PositionsHelper
+  def text_position(position)
+    "A empresa #{position.company.name} em #{position.city} está com vaga de #{position.name}. Veja mais detalhes no nosso mural. #{public_url_position(position)}"
+  end
+
+  def public_url_position(position)
+    public_position_url(position.slug)
+  end
 end
