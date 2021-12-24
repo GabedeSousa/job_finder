5047baa7e797f52378d4e8331df54b49d2c16f84
GabedeSousa <gabriel.fidelis.sousa@gmail.com>
2021-12-23 16:12:32 -0800

    applicants controller create

--- app/controllers/applicants_controller.rb
+++ app/controllers/applicants_controller.rb
@@ -4,4 +4,20 @@ class ApplicantsController < ApplicationController
 
   def new
   end
+
+  def create
+    @applicant = current_user.applicants.new(applicant_params)
+    if @applicant.save
+      flash[:notice] = 'Você acabou de aplicar para a vaga com sucesso!'
+    else
+      flash[:error] = 'Houve um erro na aplicação, por favor tente novamente.'
+    end
+    redirect_to public_position_path(@applicant.position.slug)
+  end
+
+  private
+
+  def applicant_params
+    params.require(:applicant).permit(:name, :email, :phone, :position_id)
+  end
 end
